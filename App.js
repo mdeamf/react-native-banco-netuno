286ef592c1d231bc59ddf4d1d06390fe846bfc63
mdeamf <mauriciodealmeidamf@gmail.com>
2021-05-21 17:58:10 -0300

    chore: adding safe area view

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+import { Text } from 'react-native';
+import { SafeArea } from './src/components/SafeArea';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
+      <SafeArea>
+        <Text>Banco Netuno</Text>
+      </SafeArea>
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
