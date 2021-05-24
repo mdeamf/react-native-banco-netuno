254eb2d2b9da2eba2c9475d0abeb9f5ad1704dfe
mdeamf <mauriciodealmeidamf@gmail.com>
2021-05-24 09:26:24 -0300

    feat: injecting global theme

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
 
 import { Text } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { ThemeProvider } from 'styled-components/native';
+
 import { SafeArea } from './src/components/SafeArea';
+import { theme } from './src/styles/theme';
 
 export default function App() {
   return (
     <>
-      <SafeArea>
-        <Text>Banco Netuno</Text>
-      </SafeArea>
-      <StatusBar style="auto" />
+      <ThemeProvider theme={theme}>
+        <SafeArea>
+          <Text>Banco Netuno</Text>
+        </SafeArea>
+        <StatusBar style="auto" />
+      </ThemeProvider>
     </>
   );
 }
