89ab05f79a4826f95ec3fe00cc229a71ac2ef494
mdeamf <mauriciodealmeidamf@gmail.com>
2021-05-24 13:42:54 -0300

    feat: adding basic navigation from splash to login

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
 import React from 'react';
 
-import { Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { ThemeProvider } from 'styled-components/native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import { SafeArea } from './src/components/SafeArea';
 import { theme } from './src/styles/theme';
 import { SplashPage } from './src/pages/splash/SplashPage';
+import { LoginPage } from './src/pages/login/LoginPage';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <SafeArea>
-          <SplashPage />
+          <NavigationContainer>
+            <Stack.Navigator
+              initialRouteName="Splash"
+              headerMode="none"
+              animationEnable="true"
+            >
+              <Stack.Screen name="Splash" component={SplashPage} />
+              <Stack.Screen name="Login" component={LoginPage} />
+            </Stack.Navigator>
+          </NavigationContainer>
         </SafeArea>
         <StatusBar style="auto" />
       </ThemeProvider>
--- src/pages/splash/SplashPage.js
+++ src/pages/splash/SplashPage.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { CommonActions } from '@react-navigation/native';
 
 import { FadeIn } from '../../components/FadeIn';
 import { Logo, LogoContainer } from './SplashPage.styles';
 
-export const SplashPage = () => {
+export const SplashPage = ({ navigation }) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.dispatch(
+        CommonActions.reset({
+          index: 0,
+          routes: [{ name: 'Login' }],
+        })
+      );
+    }, 6000);
+  }, [navigation]);
+
   return (
     <LogoContainer>
-      <FadeIn duration={5000}>
+      <FadeIn duration={4000}>
         <Logo source={require('../../../assets/neptune.png')} />
       </FadeIn>
     </LogoContainer>
