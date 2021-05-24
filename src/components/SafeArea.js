5bfdb581598a4c1912584a77eba53751f6e8517c
mdeamf <mauriciodealmeidamf@gmail.com>
2021-05-24 09:26:41 -0300

    feat: using global theme colors on SafeArea

--- src/components/SafeArea.js
+++ src/components/SafeArea.js
@@ -3,6 +3,6 @@ import { StatusBar, SafeAreaView } from 'react-native';
 
 export const SafeArea = styled(SafeAreaView)`
   flex: 1;
-  background-color: #ffffff;
+  background-color: ${(props) => props.theme.colors.bg.secondary};
   ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
 `;
