1fe23415e254a55be5f5300ca732346953faa8d4
mdeamf <mauriciodealmeidamf@gmail.com>
2021-06-15 18:13:33 -0300

    feat(splash): replacing dispatch with reset for navigation

--- src/pages/splash/SplashPage.js
+++ src/pages/splash/SplashPage.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import { CommonActions } from '@react-navigation/native';
 
 import { Logo } from '../../components/Logo';
 import { FadeIn } from '../../components/FadeIn';
@@ -8,12 +7,10 @@ import { LogoContainer } from './SplashPage.styles';
 export const SplashPage = ({ navigation }) => {
   useEffect(() => {
     setTimeout(() => {
-      navigation.dispatch(
-        CommonActions.reset({
-          index: 0,
-          routes: [{ name: 'Login' }],
-        })
-      );
+      navigation.reset({
+        index: 0,
+        routes: [{ name: 'Login' }],
+      });
     }, 6000);
   }, [navigation]);
 
