06c0f6b2d9099cf0afa5162cdbddd6e574228612
mdeamf <mauriciodealmeidamf@gmail.com>
2021-06-18 00:04:54 -0300

    refactor(home): grouping card components

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import { useSelector } from 'react-redux';
 import { HomeContainer, Welcome, Username } from './HomePage.styles';
-import { BalanceCard } from './components/BalanceCard';
-import { CreditCardCard } from './components/CreditCardCard';
+import { BalanceCard } from './components/Cards/BalanceCard';
+import { CreditCardCard } from './components/Cards/CreditCardCard';
 
 export default () => {
   const login = useSelector((currentState) => currentState.login);
